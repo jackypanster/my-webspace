b9eeafd20da684cd9d892bf9588182cfb621948f
panzhibin <panzhibin@gf.com.cn>
2024-02-08 13:23:39 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ IMAGE_NAME="jackypanster/my-flask-app"
 # 构建Docker镜像
 docker build -t ${IMAGE_NAME}:${TAG} .
 
+docker push ${IMAGE_NAME}:${TAG}
+
 # 运行Docker容器
 # 假设您的Flask应用监听在5000端口
-docker run -d --name ${IMAGE_NAME}-${TAG} -p 5000:5000 ${IMAGE_NAME}:${TAG}
+#docker run -d --name ${IMAGE_NAME}-${TAG} -p 5000:5000 ${IMAGE_NAME}:${TAG}
